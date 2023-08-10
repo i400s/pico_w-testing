2b25b1ccb472e317812414d2bb6303efe63284a8
Jonathan Wilson <i400s@hotmail.com>
2023-08-10 10:34:21 +0100

    RTC integration for loop time to 1sec resolution starting from an arbitrary date.

--- src/wifi_blinkwifigpio.c
+++ src/wifi_blinkwifigpio.c
@@ -1,7 +1,9 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
-#include <stdio.h>
 #include "hardware/structs/timer.h"
+#include "hardware/rtc.h"
+#include "pico/util/datetime.h"
 
 int main()
 {
@@ -11,11 +13,42 @@ int main()
 #endif
 
     stdio_init_all();
+
+    printf("Hello RTC!\n");
+
+    char datetime_buf[256];
+    char *datetime_str = &datetime_buf[0];
+
+    // Start on Friday 5th of June 2020 15:45:00
+    datetime_t t = {
+            .year  = 2020,
+            .month = 06,
+            .day   = 05,
+            .dotw  = 5, // 0 is Sunday, so 5 is Friday
+            .hour  = 15,
+            .min   = 45,
+            .sec   = 00
+    };
+
+    // Start the RTC
+    rtc_init();
+    rtc_set_datetime(&t);
+
+    // clk_sys is >2000x faster than clk_rtc, so datetime is not updated immediately when rtc_get_datetime() is called.
+    // tbe delay is up to 3 RTC clock cycles (which is 64us with the default clock settings)
+    sleep_us(64);
+
     if (cyw43_arch_init()) {
         printf("Wi-Fi init failed");
         return -1;
     }
+
     while (true) {
+
+        rtc_get_datetime(&t);
+        datetime_to_str(datetime_str, sizeof(datetime_buf), &t);
+        printf("%s      \n", datetime_str);
+
         printf("Blink LED ON\n");
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
         sleep_ms(1250);
