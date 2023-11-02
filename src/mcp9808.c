ff57ef2c3d154ef37a6a3c8b7cb7fe2e0b94cb90
Jonathan Wilson <i400s@hotmail.com>
2023-11-02 07:48:56 +0000

    Improve mcp9808 code and processing.
    Move unit specific code from header to source file
    now my understanding of function, static, scope has slightly improved.
    
    Reduce sensor count and address to 3 devices from 4 and add constant for count.
    
    Rename REG_TEMP_ register constants to a more descriptive name.
    
    Pass levels adjusted by hysteresis values to set limits function.
    
    Split processing and printing out to separate functions.
    
    Have more logical check limits code based on functional requirement.

--- src/mcp9808.c
+++ src/mcp9808.c
@@ -3,60 +3,53 @@
 #include "hardware/i2c.h"
 
 #include "src/mcp9808.h"
+#define LSB(w) ((uint8_t) ((w) & 0xFF))
+#define MSB(w) ((uint8_t) ((w) >> 8))
+static uint16_t mcp9808_calc_register(float temp);
+static void mcp9808_set_limits(uint8_t i, u_int16_t reg_frost, uint16_t reg_heating, u_int16_t reg_conditioning);
+static void mcp9808_print_temp(void);
+static void mcp9808_check_limits(uint8_t upper_byte);
+static float mcp9808_convert_temp(uint8_t upper_byte, uint8_t lower_byte);
 
 //The bus address is determined by the state of pins A0, A1 and A2 on the MCP9808 board
-const uint8_t MCP9808_ADDRESS[4] = {0x18, 0x19, 0x1A, 0x1B};
+const uint8_t MCP9808_ADDRESS[3] = {0x18, 0x1A, 0x1B};
+const uint8_t MCP9808_DEV_COUNT = 3;
 //hardware registers
 const uint8_t REG_POINTER = 0x00;
 const uint8_t REG_CONFIG = 0x01;
-const uint8_t REG_TEMP_UPPER = 0x02;
-const uint8_t REG_TEMP_LOWER = 0x03;
-const uint8_t REG_TEMP_CRIT = 0x04;
+const uint8_t REG_TEMP_FROST = 0x03; // Lower temp.
+const uint8_t REG_TEMP_HEATING = 0x02; // Upper temp.
+const uint8_t REG_TEMP_CONDITIONING = 0x04; // Critical temp.
 const uint8_t REG_TEMP_AMB = 0x05;
 const uint8_t REG_RESOLUTION = 0x08;
 
 static repeating_timer_t timer;
 
+static uint16_t mcp9808_calc_register(float temp) {
+    return (int16_t)temp << 4 | (int16_t)((temp - (int16_t)temp) / .25) << 2;
+}
+
 void mcp9808_init(gpio_irq_callback_t irq_callback, repeating_timer_callback_t timer_callback) {
 
-    float temp = 0.0;
-    int16_t whole = 0;
-    int16_t decimal = 0;
-    uint16_t result = 0;
+    // Frost protection calculation is 1°C higher for +1°C to -.5°C hysteresis.
+    float frost = 10.00;
+    uint16_t reg_frost = mcp9808_calc_register(frost + 1);
+    float adj_frost = mcp9808_convert_temp(MSB(reg_frost), LSB(reg_frost));
+    // Heating calculation is .75°C higher for +.75°C to -.75°C hysteresis.
+    float heating = 20.25;
+    uint16_t reg_heating = mcp9808_calc_register(heating + .75);
+    float adj_heating = mcp9808_convert_temp(MSB(reg_heating), LSB(reg_heating));
+    // Air conditioning calculation is 1.5°C higher for +1.5°C to -.00°C hysteresis.
+    float conditioning = 24.00;
+    uint16_t reg_conditioning = mcp9808_calc_register(conditioning + 1.5);
+    float adj_conditioning = mcp9808_convert_temp(MSB(reg_conditioning), LSB(reg_conditioning));
 
-    temp = 19.00; // LT
-    whole = (int16_t)temp << 4;
-    decimal = (int16_t)((temp - (int16_t)temp) / .25) << 2;
-    result = whole | decimal;
-    printf("LT-%2.2f whole (%d) (%x) ", temp, whole, whole);
-    printf("decimal (%d) (%x) ", decimal, decimal);
-    printf("result (%d) (%x) ", result, result);
-    result = (int16_t)temp << 4 | (int16_t)((temp - (int16_t)temp) / .25) << 2;
-    printf("full (%2.2f) (%d) (%x) \n", temp, result, result);
-
-    temp = 21.75; // LT+hysteresis(1.5)+1.25
-    whole = (int16_t)temp << 4;
-    decimal = (int16_t)((temp - (int16_t)temp) / .25) << 2;
-    result = whole | decimal;
-    printf("UT-%2.2f whole (%d) (%x) ", temp, whole, whole);
-    printf("decimal (%d) (%x) ", decimal, decimal);
-    printf("result (%d) (%x) ", result, result);
-    result = (int16_t)temp << 4 | (int16_t)((temp - (int16_t)temp) / .25) << 2;
-    printf("full (%2.2f) (%d) (%x) \n", temp, result, result);
-
-    temp = 24.00; //UT+hysteresis(1.5)+.75
-    whole = (int16_t)temp << 4;
-    decimal = (int16_t)((temp - (int16_t)temp) / .25) << 2;
-    result = whole | decimal;
-    printf("CT-%2.2f whole (%d) (%x) ", temp, whole, whole);
-    printf("decimal (%d) (%x) ", decimal, decimal);
-    printf("result (%d) (%x) ", result, result);
-    result = (int16_t)temp << 4 | (int16_t)((temp - (int16_t)temp) / .25) << 2;
-    printf("full (%2.2f) (%d) (%x) \n", temp, result, result);
+    printf("Temps: Frost(%2.2f %04X %2.2f) Heating(%2.2f %04X %2.2f) Conditioning(%2.2f %04X %2.2f) \n"
+        , frost, reg_frost, adj_frost, heating, reg_heating, adj_heating, conditioning, reg_conditioning, adj_conditioning);
 
     //
-    for (uint8_t i = 0; i < 4; i++) {
-        mcp9808_set_limits(i);
+    for (uint8_t i = 0; i < MCP9808_DEV_COUNT; i++) {
+        mcp9808_set_limits(i, reg_frost, reg_heating, reg_conditioning);
     }
 
     gpio_set_irq_enabled(MCP9808_IRQ, GPIO_IRQ_EDGE_FALL, true);
@@ -68,45 +61,33 @@ void mcp9808_init(gpio_irq_callback_t irq_callback, repeating_timer_callback_t t
 
 }
 
-void mcp9808_set_limits(uint8_t i) {
-
-    //Set a lower limit of 19.00°C for the temperature
-    uint8_t lower_temp_msb = 0x01;
-    uint8_t lower_temp_lsb = 0x30;
-
-    //Set an upper limit of 21.75°C for the temperature
-    uint8_t upper_temp_msb = 0x01;
-    uint8_t upper_temp_lsb = 0x5C;
-
-    //Set a critical limit of 24.00°C for the temperature
-    uint8_t crit_temp_msb = 0x01;
-    uint8_t crit_temp_lsb = 0x80;
+void mcp9808_set_limits(uint8_t i, u_int16_t reg_frost, uint16_t reg_heating, u_int16_t reg_conditioning) {
 
     uint8_t buf[3];
 
     printf("Init (%d", MCP9808_ADDRESS[i]);
 
-    buf[0] = REG_TEMP_LOWER;
-    buf[1] = lower_temp_msb;
-    buf[2] = lower_temp_lsb;
+    buf[0] = REG_TEMP_FROST;
+    buf[1] = MSB(reg_frost);
+    buf[2] = LSB(reg_frost);
     if (i2c_write_blocking(i2c0, MCP9808_ADDRESS[i], buf, 3, false) <0) {
         printf("*WE*");
     } else {
         printf(":LT-%02x%02x", buf[1], buf[2]);
     }
 
-    buf[0] = REG_TEMP_UPPER;
-    buf[1] = upper_temp_msb;
-    buf[2] = upper_temp_lsb;
+    buf[0] = REG_TEMP_HEATING;
+    buf[1] = MSB(reg_heating);
+    buf[2] = LSB(reg_heating);
     if (i2c_write_blocking(i2c0, MCP9808_ADDRESS[i], buf, 3, false) <0) {
         printf("*WE*");
     } else {
         printf(":UT-%02x%02x", buf[1], buf[2]);
     }
 
-    buf[0] = REG_TEMP_CRIT;
-    buf[1] = crit_temp_msb;
-    buf[2] = crit_temp_lsb;;
+    buf[0] = REG_TEMP_CONDITIONING;
+    buf[1] = MSB(reg_conditioning);
+    buf[2] = LSB(reg_conditioning);
     if (i2c_write_blocking(i2c0, MCP9808_ADDRESS[i], buf, 3, false) <0) {
         printf("*WE*");
     } else {
@@ -149,7 +130,7 @@ void mcp9808_reset_irq(void) {
     buf[0] = REG_CONFIG;
     buf[1] = 0;
     buf[2] = 0;
-    for (uint8_t i = 0; i < 4; i++) {
+    for (uint8_t i = 0; i < MCP9808_DEV_COUNT; i++) {
         printf("(%d ", MCP9808_ADDRESS[i]);
         if (i2c_write_blocking(i2c0, MCP9808_ADDRESS[i], &REG_CONFIG, 1, true) < 0) {
             printf("*WE*");
@@ -174,20 +155,21 @@ void mcp9808_reset_irq(void) {
             printf(") ");
         }
     }
-
+    printf(" \n");
+    mcp9808_print_temp();
 }
 
 void mcp9808_check_limits(uint8_t upper_byte) {
 
     // Check flags and raise alerts accordingly
-    if ((upper_byte & 0x40) == 0x40) { //TA > T-UPPER
-        printf("\033[0;32m*UL*\033[0m");
+    if ((upper_byte & 0x20) == 0x20) { // < (frost - hysteresis)
+        printf("\033[0;32m*frost*\033[0m");
     }
-    if ((upper_byte & 0x20) == 0x20) { //TA < T-LOWER
-        printf("\033[0;32m*LL*\033[0m");
+    if ((upper_byte & 0x40) != 0x40) { // < (heating) or < (heating - hysteresis)
+        printf("\033[0;32m*heat*\033[0m");
     }
-    if ((upper_byte & 0x80) == 0x80) { //TA >= T-CRIT
-        printf("\033[0;32m*CT*\033[0m");
+    if ((upper_byte & 0x80) == 0x80) { // > (conditioning)
+        printf("\033[0;32m*conditioning*\033[0m");
     }
 }
 
@@ -211,8 +193,12 @@ void mcp9808_process(repeating_timer_t *rt) {
 
     if (rt->alarm_id != timer.alarm_id) {
         return;
+    } else {
+        mcp9808_print_temp();
     }
+}
 
+void mcp9808_print_temp() {
     uint8_t buf[2];
     uint16_t upper_byte;
     uint16_t lower_byte;
@@ -221,7 +207,7 @@ void mcp9808_process(repeating_timer_t *rt) {
 
     printf("Temp: ");
 
-    for (uint8_t i = 0; i < 4; i++) {
+    for (uint8_t i = 0; i < MCP9808_DEV_COUNT; i++) {
         // Start reading ambient temperature register for 2 bytes
         if (i2c_write_blocking(i2c0, MCP9808_ADDRESS[i], &REG_TEMP_AMB, 1, true) < 0) {
             printf("*WE*");
--- src/mcp9808.h
+++ src/mcp9808.h
@@ -8,8 +8,5 @@
 void mcp9808_init(gpio_irq_callback_t irq_callback, repeating_timer_callback_t timer_callback);
 void mcp9808_reset_irq(void);
 void mcp9808_process(repeating_timer_t *rt);
-void static mcp9808_set_limits(uint8_t i);
-void static mcp9808_check_limits(uint8_t upper_byte);
-float static mcp9808_convert_temp(uint8_t upper_byte, uint8_t lower_byte);
 
 #endif
